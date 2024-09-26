25f50137a4278e070710682b84beb1d045157078
Daniel Lemire <dlemire@lemire.me>
2024-09-26 14:32:28 -0400

    various fixes

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
-#include <glfw/glfw3.h> // Will drag system OpenGL headers
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 // Loading png
 #include "lodepng.h"
